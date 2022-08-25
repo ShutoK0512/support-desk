8c0794498650333bedf42f452095515d54f5442c
角修人 <shutokaku@Shutos-Macmini.local>
2022-08-25 11:08:29 +0900

    changed file

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -5,7 +5,9 @@ const User = require('../models/userModel')
 const protect = asyncHandler(async (req, res, next) => {
     let token
 
-    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    if(
+        req.headers.authorization && 
+        req.headers.authorization.startsWith('Bearer')) {
         try {
             // Get token from header
             token = req.headers.authorization.split(' ')[1]
@@ -13,6 +15,11 @@ const protect = asyncHandler(async (req, res, next) => {
             const decoded = jwt.verify(token, process.env.JWT_SECRET)
             // Get user from token
             req.user = await User.findById(decoded.id).select('-password')
+
+            if (!req.user) {
+                res.status(401)
+                throw new Error('Not authirised')
+              }
 
             next()
         } catch (error) {
